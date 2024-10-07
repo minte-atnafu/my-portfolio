78bbd38515fc30ff669772e26210bc827aed2138
Minte <163767827+minte-atnafu@users.noreply.github.com>
2024-10-07 19:25:43 +0300

    i add motion on some commponets

--- src/componet/Contact.jsx
+++ src/componet/Contact.jsx
@@ -1,13 +1,29 @@
 import { CONTACT } from "../constants";
-
+import { motion } from "framer-motion";
 const Contact = () => {
   return (
       <div className="border-b border-neutral-900 pb-20">
-          <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
+          <motion.h1
+              whileInView={{ opacity: 1, y: 0 }}
+              initial={{ opacity: 0, y: -100 }}
+              transition={{duration:0.5}}
+              className="my-10 text-center text-4xl">Get in Touch</motion.h1>
           <div className="text-center tracking-tighter">
-              <p className="my-4">{CONTACT.address}</p>
-              <p className="my-4">{CONTACT.PhoneNo}</p>
-              <a href="#" className="border-b">{CONTACT.email}</a>
+              <motion.p
+                  whileInView={{ opacity: 1, x: 0 }}
+                  initial={{ opacity: 0, x: 100 }}
+                  transition={{duration:1}}
+                  className="my-4">{CONTACT.address}</motion.p>
+              <motion.p
+                   whileInView={{ opacity: 1, x: 0 }}
+                   initial={{ opacity: 0, x: 100 }}
+                   transition={{duration:1}}
+                  className="my-4">{CONTACT.PhoneNo}</motion.p>
+              <motion.a
+                   whileInView={{ opacity: 1, x: 0 }}
+                   initial={{ opacity: 0, x: 100 }}
+                   transition={{duration:1}}
+                  href="#" className="border-b">{CONTACT.email}</motion.a>
           </div>
       </div>
   )
--- src/componet/Project.jsx
+++ src/componet/Project.jsx
@@ -14,7 +14,7 @@ const Project = () => {
                     PROJECTS.map((project) => (
                         <div key={project.id} className="mb-8 flex flex-wrap lg:justify-center">
                             <motion.div
-                                whileInview={{ opacity: 1, x: 0 }}
+                                whileInView={{ opacity: 1, x: 0 }}
                                 initial={{ opacity: 0, x: -100 }}
                                 transition={{duration:0.5}}
                                 className="w-full lg:w-1/4">
@@ -26,13 +26,17 @@ const Project = () => {
                                     className = "mb-6 rounded"
                                 />
                             </motion.div>
-                            <div className="w-full max-w-xl lg:w-3/4">
+                            <motion.div
+                                whileInView={{ opacity: 1, x: 0 }}
+                                initial={{ opacity: 0, x: 100 }}
+                                transition={{duration:1}}
+                                className="w-full max-w-xl lg:w-3/4">
                                 <h6 className="mb-2 font-semibold">{project.title}</h6>
                                 <p className="mb-4 text-neutral-400">{project.description}</p>
                                 {project.technologies.map((tech, index) => (
                                     <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900">{tech }</span>
                                 ))}
-                            </div>
+                            </motion.div>
                         </div>
                     ))
                 ) : (
